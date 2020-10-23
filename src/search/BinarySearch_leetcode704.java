fe56a3b5f3ebe60ad252cff5abd931ceb87e1ddc
liuliu <1049505015@qq.com>
2020-10-23 09:28:04 +0800

    fix

---
+++ src/search/BinarySearch_leetcode704.java
@@ -0,0 +1,50 @@
+package search;
+
+import java.util.Arrays;
+
+public class BinarySearch_leetcode704 {
+    public static void main(String[] args) {
+
+        int[] array = new int[]{-1, 0, 3, 5, 9, 12};
+        int a = MySearch(array, 99);
+        System.out.println(a);
+    }
+
+    public static int MySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int basicBinarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            // Prevent (left + right) overflow
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // End Condition: left > right
+        return -1;
+    }
+
+}
