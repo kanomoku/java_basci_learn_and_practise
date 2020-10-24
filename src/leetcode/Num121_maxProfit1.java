cdef86f720937066be59cc2e4406b5e0782c4144
liuliu <1049505015@qq.com>
2020-10-24 21:25:50 +0800

    fix

--- src/leetcode/Num121_maxProfit1.java
+++ src/leetcode/Num121_maxProfit1.java
@@ -18,7 +18,7 @@ public class Num121_maxProfit1 {
         sell[0] = 0;
         buy[0] = -prices[0];
         for (int i = 1; i < prices.length; i++) {
-            sell[i]=Math.max(sell[i-1],prices[i]+buy[i-1]);
+            sell[i]=Math.max(sell[i-1],buy[i-1]+prices[i]);
             buy[i]=Math.max(buy[i-1],-prices[i]);
         }
         return Math.max(sell[n-1],buy[n-1]);
