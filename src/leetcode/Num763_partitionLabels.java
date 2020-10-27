15a3daf37f3be9ff68d27e127bb90a7bea7215e3
liuliu <1049505015@qq.com>
2020-10-27 21:28:52 +0800

    fix

--- src/leetcode/Num763_partitionLabels.java
+++ src/leetcode/Num763_partitionLabels.java
@@ -7,16 +7,15 @@ public class Num763_partitionLabels {
     public static void main(String[] args) {
 
         String a = "ababczbacadefegdehijhklij";
-        List<Integer> temp = partitionLabels(a);
+        List<Integer> temp = partitionLabels1(a);
         System.out.println(temp);
     }
 
-    public static List<Integer> partitionLabels(String S) {
+    public static List<Integer> partitionLabels1(String S) {
         int[] last = new int[26];
         for (int i = 0; i < S.length(); i++) {
             last[S.charAt(i) - 'a'] = i;
         }
-
         List<Integer> list = new ArrayList<>();
         int start = 0, end = 0;
         for (int i = 0; i < S.length(); i++) {
@@ -24,6 +23,24 @@ public class Num763_partitionLabels {
             if (i == end) {
                 list.add(end - start + 1);
                 start = end + 1;
+            }
+        }
+        return list;
+    }
+
+    public static List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            last[S.charAt(i) - 'a'] = Math.max(last[S.charAt(i) - 'a'], i);//数组取最大值的思路
+        }
+        List<Integer> list = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < S.length(); i++) {
+            end = Math.max(last[S.charAt(i) - 'a'], end);// 根据条件的选取右边界&&数组取最大值的思路
+            if (i == end) {
+                list.add(end - start + 1);
+                start = end + 1;//左边界根据右边的值来跳转
             }
         }
         return list;
--- src/leetcode/Num925_isLongPressedName.java
+++ src/leetcode/Num925_isLongPressedName.java
@@ -7,12 +7,12 @@ import java.util.Map;
 public class Num925_isLongPressedName {
     public static void main(String[] args) {
 
-        Boolean temp = isLongPressedName("alex", "aaleex");
+        Boolean temp = isLongPressedName2("alex", "aaleex");
         System.out.println(temp);
     }
 
 
-    public static boolean isLongPressedName(String name, String typed) {
+    public static boolean isLongPressedName1(String name, String typed) {
         int i = 0, j = 0;
         while (j < typed.length()) {
             if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
@@ -26,6 +26,25 @@ public class Num925_isLongPressedName {
         }
         return i == name.length();
     }
+
+
+    public static boolean isLongPressedName2(String name, String typed) {
+        int nameIndex = 0, typedIndex = 0;
+        //因为typed的长度肯定大于name，所以外层用typed
+        while (typedIndex < typed.length()) {
+            if (nameIndex < name.length() && name.charAt(nameIndex) == typed.charAt(typedIndex)) {
+                nameIndex++;
+                typedIndex++;
+            } else if (typedIndex > 0 && typed.charAt(typedIndex) == typed.charAt(typedIndex - 1)) {
+                typedIndex++;
+            } else {
+                return false;
+            }
+        }
+        return nameIndex == name.length();// 防止name  naaaaaa 这种情况
+    }
+
+
 }
 
 
