5b8f2bfa4b70c94105efbe44f1330891f2fb7eae
liuliu <1049505015@qq.com>
2020-11-02 19:55:56 +0800

    fix

--- src/leetcode/Num69_mySqrt_binarySearch.java
+++ src/leetcode/Num69_mySqrt_binarySearch.java
@@ -7,22 +7,23 @@ import java.util.Map;
 public class Num69_mySqrt_binarySearch {
     public static void main(String[] args) {
 
-        int temp = mySqrt1(2147395599);
+//        int temp = mySqrt3(2147395599);
+        int temp = mySqrt3(2147395599);
         System.out.println(temp);
     }
 
 
     /**
-     * 浪费性能，耗时长
+     *
      */
     public static int mySqrt(int x) {
         int l = 0, r = x;
         while (l <= r) {
             int mid = (l + r) / 2;
-            if ((long)mid * mid > x) {
+            if ((long) mid * mid > x) {
                 r = mid - 1;
             } else {
-                if ((long)(mid + 1) * (mid + 1) > x) {
+                if ((long) (mid + 1) * (mid + 1) > x) {
                     return mid;
                 }
                 l = mid + 1;
@@ -49,17 +50,33 @@ public class Num69_mySqrt_binarySearch {
     }
 
     public int mySqrt2(int x) {
-        int l=0,r=x,ans =-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(mid*mid<=x){
-                ans=mid;
-                l=mid+1;
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
             } else {
-                r=mid-1;
+                r = mid - 1;
             }
         }
         return ans;
+    }
+
+    public static int mySqrt3(int x) {
+        int res = -1;
+        int left = 0;
+        int right = x;
+        while (left <= right) {// 没有等于号的时候，输入0的时候会报错
+            int mid = (left + right) / 2;
+            if ((long) mid * mid <= x) {
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return res;
     }
 }
 
