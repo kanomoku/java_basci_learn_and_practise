925e904160039c09f87767d9e7b30f59d241ab8c
liuliu <1049505015@qq.com>
2020-11-07 14:36:14 +0800

    fix

--- src/sort/A_BubbleSort.java
+++ src/sort/A_BubbleSort.java
@@ -64,4 +64,17 @@ public class A_BubbleSort {
 //        }
 //    }
 
+
+//    public static void sort(int[] arr) {
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length - i - 1; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    int a = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = a;
+//                }
+//            }
+//        }
+//    }
+
 }
